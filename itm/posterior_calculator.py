64fe27db1ffcfd105e1bc166a7d00b10eb7a428c
fabio <fabio.chibana@gmail.com>
2022-03-19 17:29:16 +0900

    move data loading

--- itm/posterior_calculator.py
+++ itm/posterior_calculator.py
@@ -1,13 +1,36 @@
 import numpy as np
 from math import pow, pi, sqrt
 
-from itm.lcdm import H
+# from itm.lcdm import hubble
+# from itm.lcdm import H
+from itm.lcdm import LCDM
 from itm.cosmo_functions import distance_modulus, cmb, d_BAO, wigglez, fap
 
 class PosteriorCalculator:
-    def __init__(self) -> None:
-        pass
-        # load data here? create a data loader class?
+
+    def __init__(self, experiments) -> None:
+        self._experiments = experiments
+        self._cosmology = LCDM()
+        
+        # TODO: use DataLoader class
+        if 'local_hubble' in experiments:
+            print("Loading local_hubble data")
+            
+            y, y_err = np.loadtxt("data/H0.txt", comments='#', unpack=True)
+            self._local_hubble = {'y': y, 
+                                  'y_err': y_err}
+        
+        if 'cosmic_chronometers' in experiments:
+            print("Loading cosmic_chronometers data")
+            
+            x, y, y_err = np.loadtxt("data/hubble.txt", unpack=True)
+            self._cosmic_chronometers = {'x': x,
+                                         'y': y,
+                                         'y_err': y_err}
+            
+        # TODO: pass cosmology class
+            
+        
     
     def ln_posterior(self, parameters):
         ln_priors = self._ln_prior(parameters)
@@ -35,40 +58,22 @@ class PosteriorCalculator:
         Omega0_b = omega0_b/h**2
         Omega0_cdm = omega0_cdm/h**2
 
-        use_H0 = 1
-        use_cosmic_clock = 1
-        use_jla = 1
-        use_cmb = 1
-        use_bao = 1
-        use_fap = 1
+        ln_likehood = 0
 
-        lnlikehood = 0
-
-        # = H0 ====================================================================
-        if use_H0 == 0:
-            x = 0.
-            y = 0.
-            yerr = 0.
-            y, yerr = np.loadtxt("data/H0.txt", comments='#', unpack=True)
-
+        if 'local_hubble' in self._experiments:
             model = H0
-            inv_sigma2 = 1.0/yerr**2
-
-            lnlikehood += -0.5 * \
-                (np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
-
-        # = cosmic clocks =========================================================
-        if use_cosmic_clock == 0:
-            x = 0.
-            y = 0.
-            yerr = 0.
-            x, y, yerr = np.loadtxt("data/hubble.txt", unpack=True)
-
-            model = H(x, parameters)
-
-            inv_sigma2 = 1.0/yerr**2
-
-            lnlikehood += -0.5 * \
-                (np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
+            ln_likehood += self._ln_gaussian(y_fit=model,
+                                             y_target=self._local_hubble['y'],
+                                             y_err=self._local_hubble['y_err'])
         
-        return lnlikehood
+        if 'cosmic_chronometers' in self._experiments:
+            model = self._cosmology.hubble(self._cosmic_chronometers['x'], parameters)
+            ln_likehood += self._ln_gaussian(y_fit=model,
+                                             y_target=self._cosmic_chronometers['y'],
+                                             y_err=self._cosmic_chronometers['y_err'])
+        
+        return ln_likehood
+    
+    def _ln_gaussian(self, y_fit, y_target, y_err):
+        inv_sigma2 = 1.0 / y_err**2
+        return -0.5 * (np.sum((y_target - y_fit)**2 * inv_sigma2 - np.log(inv_sigma2)))
